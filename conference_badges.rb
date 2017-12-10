603b3a2f22598330bb5f6d407a5bf5fccd99908d
Robert Watson <spartan83@gmail.com>
2017-12-10 04:29:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,11 +25,11 @@ puts assign_rooms(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def printer (array)
 
-assign_rooms(array).each |Rooms| do
+assign_rooms(array).each do |Rooms|
 puts Rooms
 end
 
-batch_badge_creator(array).each |Rooms| do
+batch_badge_creator(array).each do |Rooms|
 puts Rooms
 end
 
